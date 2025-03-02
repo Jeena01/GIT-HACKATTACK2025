bbd43194db09af76eadf8a0b29a084098a1bf847
Jeena01 <jeenajavahar@gmail.com>
2025-03-01 19:16:48 -0800

    ff

--- server/app.py
+++ server/app.py
@@ -1,41 +1,47 @@
+from datetime import datetime, timedelta
 from flask import Flask, render_template, request, jsonify
 import pytesseract
 from PIL import Image
 import io
 import os
-import json 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import json
 import numpy as np
 import sqlite3
 from fuzzywuzzy import process
 
-conn = sqlite3.connect('database.db')
-
+conn = sqlite3.connect('database.db',check_same_thread=False)
+cursor = conn.cursor()
 #check if table exists
-conn.execute('''CREATE TABLE IF NOT EXISTS users
-             (userid INTEGER PRIMARY KEY AUTOINCREMENT,
-             username TEXT''')
-conn.execute('''CREATE TABLE IF NOT EXISTS inventory
+cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+             userid INTEGER PRIMARY KEY AUTOINCREMENT,
+             username TEXT,
+            password TEXT)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS inventory(
              itemid INTEGER PRIMARY KEY AUTOINCREMENT,
              userid INTEGER,
              item TEXT,
              days_till_expiry INTEGER,
              date_expiry TEXT,
+             date_purchase TEXT,
              FOREIGN KEY(userid) REFERENCES users(userid)
-             ''')
+             )''')
 
 
 app = Flask(__name__)
-
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "200"
 
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    cursor.execute("INSERT INTO users (username) VALUES (?)", (username,))
+    conn.commit()
+    return jsonify({'success': True})
 
 
 def extract_text_from_image(image):
@@ -44,7 +50,7 @@ def extract_text_from_image(image):
     lines = text.split('\n')
     return lines
 
-def parse_lines(text, threshold=80):
+def parse_lines(lines, threshold=80):
     # Load items from JSON file
     with open('expiry.json') as f:
         items = json.load(f)["perishable_grocery_items"]
@@ -53,12 +59,14 @@ def parse_lines(text, threshold=80):
     found_items = {}
 
     # Split text into words and check for fuzzy matches
-    words = text.split()
-
-    for word in words:
-        match, score = process.extractOne(word, items.keys())  # Find best match
-        if score >= threshold:  # If match is good enough
-            found_items[match] = items[match]
+    
+    for line in lines:
+        words = line.split()
+        for word in words:
+            word = word.lower()
+            match, score = process.extractOne(word, items.keys())  # Find best match
+            if score >= threshold:  # If match is good enough
+                found_items[match] = items[match]
 
     return found_items       
             
@@ -73,7 +81,7 @@ def upload_receipt():
 
     file = request.files['file']
     userid = request.form['userid']
-    
+  
 
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
@@ -89,9 +97,15 @@ def upload_receipt():
         text = extract_text_from_image(image)
         found_items = parse_lines(text)
 
+        print(found_items, flush=True)
+
+        date_purchase = datetime.today().date()
+        
         for item in found_items:
-            conn.execute("INSERT INTO inventory (userid, item, days_till_expiry, date_expiry) VALUES (?, ?, ?, ?)", (1, item, found_items[item], "2023-12-31"))
+
+            conn.execute("INSERT INTO inventory (userid, item, days_till_expiry, date_expiry, date_purchase) VALUES (?, ?, ?, ?,?)", (userid, item, found_items[item], "2023-12-31","2023-12-12"))
             conn.commit()
+        return jsonify({'success': True})
     
 @app.route('/remove', methods=['POST'])
 def remove_item():
@@ -107,3 +121,9 @@ def perishables():
     cursor = conn.execute("SELECT * FROM inventory WHERE userid = ?", (userid, ))
     items = cursor.fetchall()
     return jsonify({'items': items})
+
+
+if __name__ == '__main__':
+
+
+    app.run()
